017376897c2a92c346c8027b2ee62f2818867e62
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-03-07 21:41:10 -0500

    refactor and added form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def show  
   end
 
   def index
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -16,7 +17,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
@@ -25,13 +26,27 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+  def update 
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully"
       redirect_to @article
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
